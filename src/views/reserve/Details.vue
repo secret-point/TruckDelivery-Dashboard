75ac7b5430c071ed568db2e30cb180f641991a6c
secret-point <secret.point2020@gmail.com>
2023-03-28 15:20:26 +0530

    add what will you get UI

--- src/views/reserve/Details.vue
+++ src/views/reserve/Details.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="text-body-2">
-    <p class="font-weight-black my-5">[If booked, see out other listing by visiting our profile]</p>
-    <p class="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam</p>
-    <p class="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eriod tempor...</p>
-    <p class="font-weight-black mt-5">John is a superhost</p>
-    <p class="my-3">superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p>
-    <p class="font-weight-black mt-5">Free cancellation before May 9.</p>
-    <p class="my-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam</p>
+    <!-- <p class="font-weight-black my-5">[If booked, see out other listing by visiting our profile]</p> -->
+    <!-- <p class="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam</p> -->
+    <!-- <p class="my-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eriod tempor...</p> -->
+    <!-- <p class="font-weight-black mt-5">John is a superhost</p> -->
+    <!-- <p class="my-3">superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</p> -->
+    <!-- <p class="font-weight-black mt-5">Free cancellation before May 9.</p> -->
+    <!-- <p class="my-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam</p> -->
     <div class="my-6">
       <h6 class=" font-weight-black my-4 text-body-2">What will you get</h6>
       <img src="@/assets/trucks.png" alt="" width="308" height="129">
