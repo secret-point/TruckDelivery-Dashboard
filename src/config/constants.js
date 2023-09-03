6ddc7da2d5875e8b788efdccc701c8be027aa8b6
secret-point <secret.point2020@gmail.com>
2023-09-03 00:04:55 +0000

    Merged in truckingDirectoryFe0902-01 (pull request #37)
    Basic constants
    
    * Basic constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -3,31 +3,85 @@ export default {
    * Google Map Autocomplete Options
    */
   googleMapAutoCompleteOptions: {
-    fields: ["geometry", "formatted_address", "address_components"],
+    fields: ['geometry', 'formatted_address', 'address_components'],
   },
 
   roles: [
     {
-      label: "Carrier",
-      value: "carrier",
+      label: 'Carrier',
+      value: 'carrier',
     },
     {
-      label: "Broker",
-      value: "broker", // TODO: must change
+      label: 'Broker',
+      value: 'broker', // TODO: must change
     },
     {
-      label: "Shipper",
-      value: "shipper", // TODO: must change
+      label: 'Shipper',
+      value: 'shipper', // TODO: must change
     },
   ],
   allowedRegisterRoles: [
     {
-      label: "Broker",
-      value: "broker", // TODO: must change
+      label: 'Broker',
+      value: 'broker', // TODO: must change
     },
     {
-      label: "Shipper",
-      value: "shipper", // TODO: must change
+      label: 'Shipper',
+      value: 'shipper', // TODO: must change
     },
   ],
+
+  usStates: {
+    AL: 'Alabama',
+    AK: 'Alaska',
+    AZ: 'Arizona',
+    AR: 'Arkansas',
+    CA: 'California',
+    CO: 'Colorado',
+    CT: 'Connecticut',
+    DC: 'Washington, D.C.',
+    DE: 'Delaware',
+    FL: 'Florida',
+    GA: 'Georgia',
+    HI: 'Hawaii',
+    ID: 'Idaho',
+    IL: 'Illinois',
+    IN: 'Indiana',
+    IA: 'Iowa',
+    KS: 'Kansas',
+    KY: 'Kentucky',
+    LA: 'Louisiana',
+    ME: 'Maine',
+    MD: 'Maryland',
+    MA: 'Massachusetts',
+    MI: 'Michigan',
+    MN: 'Minnesota',
+    MS: 'Mississippi',
+    MO: 'Missouri',
+    MT: 'Montana',
+    NE: 'Nebraska',
+    NV: 'Nevada',
+    NH: 'New Hampshire',
+    NJ: 'New Jersey',
+    NM: 'New Mexico',
+    NY: 'New York',
+    NC: 'North Carolina',
+    ND: 'North Dakota',
+    OH: 'Ohio',
+    OK: 'Oklahoma',
+    OR: 'Oregon',
+    PA: 'Pennsylvania',
+    RI: 'Rhode Island',
+    SC: 'South Carolina',
+    SD: 'South Dakota',
+    TN: 'Tennessee',
+    TX: 'Texas',
+    UT: 'Utah',
+    VT: 'Vermont',
+    VA: 'Virginia',
+    WA: 'Washington',
+    WV: 'West Virginia',
+    WI: 'Wisconsin',
+    WY: 'Wyoming',
+  },
 };
--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,19 @@
 // const isProd = true;
 const Config = {
-    SERVICE_URL: "http://127.0.0.1:8000/api/",
-    IS_PROD: false,
-    MIX_GOOGLE_MAP_API_KEY: 'AIzaSyBSWgph_L-xkYyn9R--oUNQ7ze2cIBrXds'
-  };
-  
-  const ProdConfig = {
-    SERVICE_URL: "https://dashboard.ezpapel.ai/api/",
-    MIX_GOOGLE_MAP_API_KEY: 'AIzaSyBSWgph_L-xkYyn9R--oUNQ7ze2cIBrXds',
-    IS_PROD: true,
-  };
-  
-  /* Decide config values based upon build type: Debug/Release */
-  export default process.env.NODE_ENV && process.env.NODE_ENV === "development"
-    ? Config
-    : ProdConfig;
-  
+  SERVICE_URL: 'http://127.0.0.1:8000/api/',
+  IS_PROD: false,
+  MIX_GOOGLE_MAP_API_KEY: 'AIzaSyBSWgph_L-xkYyn9R--oUNQ7ze2cIBrXds',
+  NODE_SERVER_URL: 'http://127.0.0.1:3000/api/',
+};
+
+const ProdConfig = {
+  SERVICE_URL: 'https://dashboard.ezpapel.ai/api/',
+  MIX_GOOGLE_MAP_API_KEY: 'AIzaSyBSWgph_L-xkYyn9R--oUNQ7ze2cIBrXds',
+  IS_PROD: true,
+  NODE_SERVER_URL: 'https://api23.ezpapel.ai/api/',
+};
+
+/* Decide config values based upon build type: Debug/Release */
+export default process.env.NODE_ENV && process.env.NODE_ENV === 'development'
+  ? Config
+  : ProdConfig;
