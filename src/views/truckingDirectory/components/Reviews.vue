07d1e091be551b8deded76e7c7a2ee190547984a
secret-point <secret.point2020@gmail.com>
2023-09-03 03:43:59 +0000

    Merged in truckingDirectoryFe0902-04 (pull request #41)
    Safety & Reviews UI, not used for now
    
    * Safety & Reviews UI, not used for now

---
+++ src/views/truckingDirectory/components/Reviews.vue
@@ -0,0 +1,110 @@
+<template>
+  <v-card>
+    <v-card-title class="card-title">Reviews</v-card-title>
+    <v-card-text class="card-content">
+      <v-row>
+        <v-col cols="12">
+          <p></p>
+        </v-col>
+      </v-row>
+      <v-list>
+        <template v-for="(item, index) in items" :key="item.title">
+          <div class="review-section">
+            <v-list-item
+              :prepend-avatar="item.prependAvatar"
+              :title="item.title"
+            >
+              <template v-slot:title>
+                <p>
+                  <span>
+                    {{ item.title }}
+                  </span>
+                  <span class="review-posted-date">
+                    Posted on {{ item.posted_date }}
+                  </span>
+                </p>
+              </template>
+              <template v-slot:subtitle>
+                <v-rating
+                  :model-value="item.rating"
+                  readonly
+                  half-increments
+                  density="compact"
+                />
+              </template>
+            </v-list-item>
+            <v-list-item>
+              {{ item.content }}
+            </v-list-item>
+          </div>
+        </template>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Reviews',
+  data() {
+    return {
+      items: [
+        {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'John Smith',
+          rating: 3,
+          posted_date: 'July 22, 2023',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada nisi eu libero fermentum sollicitudin. Donec vel eleifend dolor, eget semper urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+        },
+        {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'John Smith',
+          rating: 3.5,
+          posted_date: 'July 22, 2023',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada nisi eu libero fermentum sollicitudin. Donec vel eleifend dolor, eget semper urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+        },
+        {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'John Smith',
+          rating: 4.5,
+          posted_date: 'July 22, 2023',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada nisi eu libero fermentum sollicitudin. Donec vel eleifend dolor, eget semper urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+        },
+        {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'John Smith',
+          rating: 3.5,
+          posted_date: 'July 22, 2023',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada nisi eu libero fermentum sollicitudin. Donec vel eleifend dolor, eget semper urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+        },
+        {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'John Smith',
+          rating: 3.2,
+          posted_date: 'July 22, 2023',
+          content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada nisi eu libero fermentum sollicitudin. Donec vel eleifend dolor, eget semper urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.review-section {
+  border-radius: 8px;
+  border: 1px solid #3975eb26;
+  margin: 16px 0px;
+  padding: 16px 0px;
+
+  .review-posted-date {
+    color: #bcbcbc;
+    float: right;
+  }
+}
+</style>
---
+++ src/views/truckingDirectory/components/Safety.vue
@@ -0,0 +1,168 @@
+<template>
+  <v-card>
+    <v-card-title class="card-title">Safety Ratings</v-card-title>
+    <v-card-text class="card-content">
+      <v-row>
+        <v-col cols="2">
+          <h6>
+            {{ companyInfo.dotNumber }}
+            <!-- {{ companyInfo.dotNumber }} - {{ companyInfo.mcNumber }} -->
+          </h6>
+          <h6>
+            {{ companyInfo.legalName }}
+          </h6>
+        </v-col>
+        <v-col cols="10" class="safety-section">
+          <v-row>
+            <v-col cols="12">
+              <v-list-item
+                :prepend-avatar="items.total.prependAvatar"
+                :title="items.total.title"
+                :subtitle="items.total.subtitle"
+              >
+              </v-list-item>
+            </v-col>
+          </v-row>
+          <v-row class="category-list">
+            <v-col cols="6">
+              <div class="category-list-container">
+                <div
+                  v-for="(item, index) in items.categories"
+                  :key="index"
+                  class="category-list-item"
+                >
+                  <v-img
+                    :src="item.prependAvatar"
+                    alt="Image"
+                    class="item-image"
+                  >
+                    <template v-slot:placeholder>
+                      <div class="blank-logo"></div>
+                    </template>
+                  </v-img>
+                  <p class="category-item-title">{{ item.title }}</p>
+                </div>
+              </div>
+            </v-col>
+            <v-col cols="6">
+              <v-table class="pa-4">
+                <thead>
+                  <tr>
+                    <th>Severity</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Moderate</td>
+                    <td>Safety</td>
+                    <td>Carrier has BASIC alert in one category.</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Safety',
+  data() {
+    return {
+      items: {
+        total: {
+          prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Overall assessment using checklist',
+          subtitle: 'Moderate',
+        },
+        categories: [
+          {
+            prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/7.jpg',
+            title: 'Authority',
+          },
+          {
+            prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+            title: 'Insurance',
+          },
+          {
+            prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+            title: 'Safety/CSA-e',
+          },
+          {
+            prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+            title: 'Operations',
+          },
+          {
+            prependAvatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+            title: 'Other',
+          },
+        ],
+      },
+    };
+  },
+  computed: {
+    companyInfo() {
+      return this.$store.state.fmcsaCarrier.selectedFmcsaCarrier;
+    },
+  },
+};
+</script>
+
+<style>
+.category-list-container {
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+}
+
+.category-list-item {
+  text-align: center;
+}
+
+.item-image {
+  width: 56px;
+  height: 56px;
+  object-fit: cover;
+  border-radius: 50%;
+  margin: auto;
+}
+
+.category-item-title {
+  margin-top: 10px;
+  font-size: 14px;
+}
+
+.card-section {
+  border-radius: 8px;
+  border: 1px solid #3975eb26;
+  margin: 16px 0px;
+  padding: 16px 0px;
+}
+
+.safety-section {
+  .v-table > .v-table__wrapper > table {
+    border-collapse: collapse;
+  }
+
+  .v-table > .v-table__wrapper > table > tbody > tr > td,
+  .v-table > .v-table__wrapper > table > tbody > tr > th,
+  .v-table > .v-table__wrapper > table > thead > tr > td,
+  .v-table > .v-table__wrapper > table > thead > tr > th,
+  .v-table > .v-table__wrapper > table > tfoot > tr > td,
+  .v-table > .v-table__wrapper > table > tfoot > tr > th {
+    padding: 0px;
+    height: 30px;
+    border: 1px solid #e8e8e8;
+    font-size: 14px;
+  }
+
+  .v-table > .v-table__wrapper > table > thead > tr > th {
+    background-color: #f6f6f6;
+  }
+}
+</style>
